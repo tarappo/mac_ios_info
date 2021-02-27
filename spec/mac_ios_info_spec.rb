fd1507be20d5f301dc0b0df958caa5539f530c5a
tarappo <tarappo@gmail.com>
2021-02-28 08:05:34 +0900

    add first simple test code

--- spec/mac_ios_info_spec.rb
+++ spec/mac_ios_info_spec.rb
@@ -3,5 +3,16 @@ RSpec.describe MacIosInfo do
     expect(MacIosInfo::VERSION).not_to be nil
   end
 
+  it "macOS Name" do
+    expect(MacIosInfo.macos_version_to_os_name(version: "11.1.1")).to eq("macOS Big Sur")
+    expect(MacIosInfo.macos_version_to_os_name(version: "10.15.4")).to eq("macOS Catalina")
+    expect(MacIosInfo.macos_version_to_os_name(version: "10.14.4")).to eq("macOS Mojave")
+    expect(MacIosInfo.macos_version_to_os_name(version: "10.13.4")).to eq("macOS High Sierra")
+    expect(MacIosInfo.macos_version_to_os_name(version: "10.12.4")).to eq("macOS Sierra")
+    expect(MacIosInfo.macos_version_to_os_name(version: "10.11.1")).to eq("UnKnown")
+  end
 
+  it "macOS Version" do
+    expect(MacIosInfo.macos_build_to_macos_version(build_number: "20D64")).to eq("11.2")
+  end
 end
